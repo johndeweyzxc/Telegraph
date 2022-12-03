baa29e92f036887bcf1b5f746f67683aaf97c48d
johndeweyzxc <johndewey02003@gmail.com>
2022-12-04 00:05:21 +0800

    Added defaultBlue and loginSecondaryPadding

--- lib/const_var.dart
+++ lib/const_var.dart
@@ -4,12 +4,13 @@ const productColor = Color.fromARGB(255, 99, 43, 255);
 const defaultGrey = Color.fromARGB(167, 129, 129, 129);
 const defaultWhite = Color.fromARGB(255, 255, 255, 255);
 const defaultBlack = Color.fromARGB(146, 0, 0, 0);
+const defaultBlue = Colors.blue;
 
 // Login page constants
 const double loginTextSizeBig = 20.0;
 const double logintTextSizeSmall = 16.0;
 const loginDefaultPadding = EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 0.0);
-const loginSecondaryPadding = EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0);
+const loginSecondaryPadding = EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0);
 
 double widthScreen(context) {
   return MediaQuery.of(context).size.width;
