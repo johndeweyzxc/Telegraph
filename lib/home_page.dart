c0b187b3e5795111fc780c4306fd996f223dcb81
johndeweyzxc <johndewey02003@gmail.com>
2022-12-04 00:08:56 +0800

    Created Home page as the main page of the app

---
+++ lib/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:telegraph/google_auth.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            GoogleAuth().signOut();
+          },
+          child: const Text("Log out"),
+        ),
+      ),
+    );
+  }
+}
