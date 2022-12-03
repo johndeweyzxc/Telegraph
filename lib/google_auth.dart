df84d4ebc88b1d4a7c5321b9a8741cc6ebb14616
johndeweyzxc <johndewey02003@gmail.com>
2022-12-04 00:06:49 +0800

    Created google_auth.dart file to handle authentication with firebase

---
+++ lib/google_auth.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:firebase_auth/firebase_auth.dart';
+
+class GoogleAuth {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => _firebaseAuth.currentUser;
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
