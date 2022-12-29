0f6baf300f2d25d57ba5b98c7e759c3936caa9b9
johndeweyzxc <johndewey02003@gmail.com>
2022-12-29 16:19:05 +0800

    Changed color of black

--- lib/const_var.dart
+++ lib/const_var.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 const deepPurple500 = Color(0xff673ab7);
 const grey500 = Color(0xff9e9e9e);
 const white = Color(0xffffffff);
-const black = Color(0xff000000);
+const black = Color(0xff424242);
 const lightBlue600 = Color(0xff039be5);
 
 const double textbig = 20.0;
